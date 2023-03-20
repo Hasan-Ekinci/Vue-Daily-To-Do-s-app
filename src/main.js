59ff3c2e5a71c6ebf9e8314810a7b60971f60051
s1123069 <s1123069@student.hsleiden.nl>
2023-03-20 17:47:53 +0100

    installed vue router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// Router imports
+// import Vue from "vue";
+// import VueRouter from "vue-router";
+
+createApp(App).mount("#app");
+
+// const app = createApp(App);
+
+// const router = createRouter({
+//     history: createWebHistory(),
+//     routes: [
+//         {path: '/teams', component: Teamslist, props: true},
+//         {path: '/users', component: UsersList, props: true}
+//     ]
+// });
+
+// app.use(router);
+
+// app.mount('#app');
