18b13d56a5b0001bc9a1de3657aafbfe6affa1e8
s1123069 <s1123069@student.hsleiden.nl>
2023-03-21 15:00:48 +0100

    removed everything for store

--- src/components/IndexComponent.vue
+++ src/components/IndexComponent.vue
@@ -1,32 +1,14 @@
 <template>
-  <p>{{ counter }}</p>
-  <button @click="plusOne">Doe plus 1</button>
-  <button @click="plusTwo">Doe plus 2</button>
+  <p>Hoi</p>
 </template>
 
 <script>
-import { useStore } from "vuex";
-import { computed } from "vue";
-
 export default {
   name: "IndexComponent",
   setup() {
-    const store = useStore()
-
-    const counter = computed(() => store.state.counter)
-
-    function plusOne() {
-      store.commit('plusOne')
-    }
-
-    function plusTwo() {
-      store.commit('plusCustom', 2)
-    }
 
     return {
-      counter,
-      plusOne,
-      plusTwo
+      
     };
   },
 };
