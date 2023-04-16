41d5d904be7c44efb87f365e5b08626c378ca6ec
Hasan Ekinci <hasancan_ekinci66@hotmail.com>
2023-04-16 16:07:53 +0200

    re-added stuff from store to indexcomoponent

--- src/components/IndexComponent.vue
+++ src/components/IndexComponent.vue
@@ -1,14 +1,32 @@
 <template>
-  <p>Hoi</p>
+  <p>{{ counter }}</p>
+  <button @click="plusOne">Doe plus 1</button>
+  <button @click="plusTwo">Doe plus 2</button>
 </template>
 
 <script>
+import { useStore } from "vuex";
+import { computed } from "vue";
+
 export default {
   name: "IndexComponent",
   setup() {
+    const store = useStore()
+
+    const counter = computed(() => store.state.counter)
+
+    function plusOne() {
+      store.commit('plusOne')
+    }
+
+    function plusTwo() {
+      store.commit('plusCustom', 2)
+    }
 
     return {
-      
+      counter,
+      plusOne,
+      plusTwo
     };
   },
 };
