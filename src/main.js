0170edca5bf72b5bf6bba7417c9f2e1b2fcf1d29
Hasan Ekinci <hasancan_ekinci66@hotmail.com>
2023-04-16 16:14:24 +0200

    added code for store to main.js. Store is now back

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from "vue";
 import router from "./router.js";
 import App from "./App.vue";
-
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import store from "./store.js";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 const app = createApp(App);
-app.use(VueAxios, axios)
+app.use(VueAxios, axios);
 app.use(router);
+app.use(store);
 
 app.mount("#app");
