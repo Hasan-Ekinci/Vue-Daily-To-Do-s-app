8ed12ae35dca5834331e65734720f6efbdb22ffa
Hasan Ekinci <hasancan_ekinci66@hotmail.com>
2023-04-20 19:55:18 +0200

    did styling for login page

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,12 +1,17 @@
 <template>
-  <form @submit.prevent="authenticate">
-    <h1>Login</h1>
-    <h2>Email</h2>
-    <input type="text" v-model="email" />
-    <h2>Wachtwoord</h2>
-    <input type="password" v-model="password" />
-    <button type="submit">Login</button>
-  </form>
+  <div class="loginWrapper">
+    <h1>Daily To Do's</h1>
+
+    <form class="loginForm" @submit.prevent="authenticate">
+      <h2>Login</h2>
+      <h3>Email</h3>
+      <input type="email" v-model="email" />
+      <h3>Wachtwoord</h3>
+      <input type="password" v-model="password" />
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -51,3 +56,68 @@ export default {
   },
 };
 </script>
+
+<style>
+.loginWrapper {
+  margin: 0 auto;
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.loginForm {
+  align-self: center;
+}
+
+.loginWrapper h1 {
+  font-size: 4rem;
+  margin-bottom: 5rem;
+  align-self: center;
+  background: rgb(73, 72, 70);
+  background: linear-gradient(
+    145deg,
+    rgba(73, 72, 70, 1) 0%,
+    rgba(145, 144, 141, 1) 50%,
+    rgba(73, 72, 70, 1) 100%
+  );
+  padding: 0.5rem;
+  border-radius: 1rem;
+  color: white;
+}
+
+.loginForm h2 {
+  font-size: 3rem;
+  margin-bottom: 2rem;
+}
+
+.loginForm h3 {
+  font-size: 2rem;
+}
+
+.loginForm input {
+  margin: 0.5rem auto 3rem;
+  font-size: 2rem;
+  width: 40rem;
+  padding: 0.5rem;
+  border-radius: 1rem;
+}
+
+.loginForm button {
+  margin: 0 auto;
+  display: block;
+  background-color: var(--light-green);
+  font-size: 2rem;
+  padding: 1rem;
+  border-radius: 1rem;
+}
+
+.loginForm button:hover {
+  background-color: var(--light-gray);
+}
+
+.loginForm button:active {
+  background-color: var(--dark-gray);
+  color: white;
+}
+</style>
